33dceeb0da199db7e055a0d035a441e122ba48ab
Samuel De La Cruz <samuel@codetiger.co>
2024-01-11 07:59:48 -0400

    Add file deletion functionality with logging deleted files

--- fileSystem/walk/main.go
+++ fileSystem/walk/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"path/filepath"
 )
@@ -15,22 +16,41 @@ type config struct {
 	size int64
 	// list files
 	list bool
+	del  bool
+	// Log destination writer
+	wLog io.Writer
 }
 
 func main() {
 	// Parse command line flags
 	root := flag.String("root", ".", "Root directory to start scanning")
+	logFile := flag.String("log", "", "Log file to write deleted files")
 	// Action options
 	list := flag.Bool("list", false, "List files only")
 
+	del := flag.Bool("del", false, "Delete files")
 	// Filter options
 	ext := flag.String("ext", "", "File extension to filter out")
 	size := flag.Int64("size", 0, "Minimum file size to filter out")
 	flag.Parse()
+	var (
+		f   = os.Stdout
+		err error
+	)
+	if *logFile != "" {
+		f, err = os.OpenFile(*logFile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		defer f.Close()
+	}
 	c := config{
 		ext:  *ext,
 		size: *size,
 		list: *list,
+		del:  *del,
+		wLog: f,
 	}
 	if err := run(*root, os.Stdout, c); err != nil {
 		fmt.Fprintln(os.Stderr, err)
@@ -39,6 +59,7 @@ func main() {
 }
 
 func run(root string, out io.Writer, cfg config) error {
+	delLogger := log.New(cfg.wLog, "DELETED FILE: ", log.LstdFlags)
 	return filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			// Return error to stop walking
@@ -50,6 +71,9 @@ func run(root string, out io.Writer, cfg config) error {
 		// if list was explicitly set to true, dont do anything else
 		if cfg.list {
 			return listFile(path, out)
+		}
+		if cfg.del {
+			return delFile(path, delLogger)
 		}
 		// List is the default option if nothing else is specified
 		return listFile(path, out)
