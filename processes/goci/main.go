6a3a4be2bbefd7cfd329fa53410f5c8147d5ff6e
Samuel De La Cruz <samuel@codetiger.co>
2024-01-27 08:45:38 -0400

    Add timeoutStep

--- processes/goci/main.go
+++ processes/goci/main.go
@@ -24,11 +24,13 @@ func run(proj string, out io.Writer) error {
 	if proj == "" {
 		return fmt.Errorf("project directory is required: %w", ErrValidation)
 	}
+
 	pipeline := make([]executer, 3)
+	//pipeline := make([]executer, 4)
 	pipeline[0] = newStep("go build", "go", "Go Build: SUCCESS", proj, []string{"build", ".", "errors"})
 	pipeline[1] = newStep("go test", "go", "Go Test: SUCCESS", proj, []string{"test", "-v"})
 	pipeline[2] = newExceptionStep("go fmt", "gofmt", "Gofmt: SUCCESS", proj, []string{"-l", "."})
-
+	//pipeline[3] = newTimeoutStep("git push", "git", "Git Push: SUCCESS", proj, []string{"push", "origin", "master"}, 10*time.Second)
 	for _, s := range pipeline {
 		msg, err := s.execute()
 		if err != nil {
