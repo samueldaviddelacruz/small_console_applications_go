461d640ef54141209ca0fb785175ebabef8be3b6
Samuel De La Cruz <samuel@codetiger.co>
2024-01-28 08:26:52 -0400

    Add git push step to pipeline

--- processes/goci/main_test.go
+++ processes/goci/main_test.go
@@ -3,22 +3,35 @@ package main
 import (
 	"bytes"
 	"errors"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"testing"
 )
 
 func TestRun(t *testing.T) {
+	_, err := exec.LookPath("git")
+	if err != nil {
+		t.Skip("git not installed, skipping test")
+	}
 	var testCases = []struct {
-		name   string
-		proj   string
-		out    string
-		expErr error
+		name     string
+		proj     string
+		out      string
+		expErr   error
+		setupGit bool
 	}{
-		{name: "success", proj: "./testdata/tool", out: "Go Build: SUCCESS\nGo Test: SUCCESS\nGofmt: SUCCESS\n", expErr: nil},
-		{name: "fail", proj: "./testdata/toolErr", out: "", expErr: &stepError{step: "go build"}},
-		{name: "failFormat", proj: "./testdata/toolFmtErr", out: "", expErr: &stepError{step: "go fmt"}},
+		{name: "success", proj: "./testdata/tool", out: "Go Build: SUCCESS\nGo Test: SUCCESS\nGofmt: SUCCESS\nGit Push: SUCCESS\n", expErr: nil, setupGit: true},
+		{name: "fail", proj: "./testdata/toolErr", out: "", expErr: &stepError{step: "go build"}, setupGit: false},
+		{name: "failFormat", proj: "./testdata/toolFmtErr", out: "", expErr: &stepError{step: "go fmt"}, setupGit: false},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
+			if tc.setupGit {
+				cleanup := setupGit(t, tc.proj)
+				defer cleanup()
+			}
 			var out bytes.Buffer
 			err := run(tc.proj, &out)
 			if tc.expErr != nil {
@@ -38,5 +51,52 @@ func TestRun(t *testing.T) {
 				t.Errorf("Expected output : %q, got %q instead", tc.out, out.String())
 			}
 		})
+	}
+}
+
+func setupGit(t *testing.T, proj string) func() {
+	t.Helper()
+	gitExec, err := exec.LookPath("git")
+	if err != nil {
+		t.Fatal(err)
+	}
+	tempDir, err := os.MkdirTemp("", "gocitest")
+	if err != nil {
+		t.Fatal(err)
+	}
+	projPath, err := filepath.Abs(proj)
+	if err != nil {
+		t.Fatal(err)
+	}
+	remoteURI := fmt.Sprintf("file://%s", tempDir)
+	var gitCmdList = []struct {
+		args []string
+		dir  string
+		env  []string
+	}{
+		{[]string{"init", "--bare"}, tempDir, nil},
+		{[]string{"init"}, projPath, nil},
+		{[]string{"remote", "add", "origin", remoteURI}, projPath, nil},
+		{[]string{"add", "."}, projPath, nil},
+		{[]string{"commit", "-m", "test"}, projPath, []string{
+			"GIT_AUTHOR_NAME=test",
+			"GIT_COMMITTER_EMAIL=test@example.com",
+			"GIT_COMMITTER_NAME=Test",
+			"GIT_AUTHOR_EMAIL=test@example.com",
+		}},
+	}
+	for _, g := range gitCmdList {
+		gitCmd := exec.Command(gitExec, g.args...)
+		gitCmd.Dir = g.dir
+		if g.env != nil {
+			gitCmd.Env = append(os.Environ(), g.env...)
+		}
+		if err := gitCmd.Run(); err != nil {
+			t.Fatal(err)
+		}
+	}
+	return func() {
+		os.RemoveAll(tempDir)
+		os.RemoveAll(filepath.Join(projPath, ".git"))
 	}
 }
